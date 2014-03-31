d196959f34dcd01c4716a9ee857dd3885e1bb3f2
LeMinhThanh <le.minh.thanh@framgia.com>
2014-03-31 11:06:23 +0700

    Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = [
+  ["admin@gmail.com", "123456"],
+  ["user1@gmail.com", "123456"],
+  ["user2@gmail.com", "123456"],
+  ["user3@gmail.com", "123456"],
+  ["user4@gmail.com", "123456"],
+  ["user5@gmail.com", "123456"],
+]
+users.each do |u|
+  User.create(email: u[0], password: u[1])
+end
