429c53ecbfb98573b078f20d1361b115534395dd
LeMinhThanh <le.minh.thanh@framgia.com>
2014-03-31 10:39:36 +0700

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeginningRails4::Application.config.secret_key_base = 'd2f135d95fbb30a5ac85feceefc994c90cf00c1864bfde1ca2e593d916ed75ae3e5917670842f7a7e3c174775e85722487ec3947d3d642c9b99f5ba094132c46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeginningRails4::Application.config.session_store :cookie_store, key: '_BeginningRails4_session'
